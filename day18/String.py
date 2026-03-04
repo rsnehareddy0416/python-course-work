c0ff7e0d9237d150dbd8e18e45babec6397cbb2d
rsnehareddy0416 <r.snehareddy04@gmail.com>
2026-03-04 13:09:01 +0530

    Add files via upload

---
+++ day18/String.py
@@ -0,0 +1,16 @@
+'''import operations
+operations.add(6,7)
+operations.sub(6,7)
+operations.mul(6,7)
+operations.div(6,7)'''
+
+'''from operations import add,sub
+add(6,7)
+sub(6,7)'''
+
+from Operations import *
+add(6,7)
+sub(6,7)
+mul(6,7)
+div(6,7)
+
